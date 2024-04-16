980e4c6dba47ec92a0b76460b3d9b5b9fde83404
Thomas Luca <tluca@frba.utn.edu.ar>
2024-04-16 01:54:08 +0000

    Inicie memoria, se conecto la cpu,mando msj, se desconecto, se conecto el kernel, mando msj, se deconecto Y EL SERVER NO SE CAYO, REEEEEEETOP

--- cpu/src/main.c
+++ cpu/src/main.c
@@ -20,19 +20,19 @@ int main(int argc, char* argv[])
 	log_info(logger_CPU, "Iniciando CPU...");
 	
     conexion_cpu_memoria = crear_conexion(ip_memoria, puerto_memoria);
-	enviar_mensaje("Mensaje de CPU para memoria", conexion);
-	paquete(conexion, logger_CPU);
+	enviar_mensaje("Mensaje de CPU para memoria", conexion_cpu_memoria);
+	paquete(conexion_cpu_memoria, logger_CPU);
 
-    int server_dispatch = iniciar_servidor(logger_CPU, "CPU", ip_cpu, puerto_escucha_dispatch);
+    /*int server_dispatch = iniciar_servidor(logger_CPU, "CPU", ip_cpu, puerto_escucha_dispatch);
 	log_info(logger_CPU, "CPU listo para recibir al cliente");
-	procesar_conexion(server_dispatch, logger_CPU);
+	procesar_conexion(server_dispatch, logger_CPU);*/
 
     /*int server_interrupt = iniciar_servidor(logger_CPU, "CPU", ip_cpu, puerto_escucha_interrupt);
 	log_info(logger_CPU, "CPU listo para recibir al cliente");
 	procesar_conexion(server_interrupt, logger_CPU);*/
 
 
-	//terminar_programa(conexion, logger_CPU, config);
+	terminar_programa(conexion_cpu_memoria, logger_CPU, config);
 }
 
 void inicializar_config(void){
